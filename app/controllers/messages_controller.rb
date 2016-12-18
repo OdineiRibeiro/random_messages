9bbffef983033c881c27e1e6f004411c442db8de
Odinei Rocha <odinei.ribeiro@live.com>
2016-12-18 13:06:25 -0200

    is almost possible create messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,4 +6,24 @@ class MessagesController < ApplicationController
   def show
     @message = Message.find(params['id'])
   end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+
+    if @message.save!
+
+    else
+
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:title, :text)
+  end
 end
