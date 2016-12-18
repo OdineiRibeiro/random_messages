a8f6cbb9fb63607de896b4e50d949e2003bfcb18
Odinei Rocha <odinei.ribeiro@live.com>
2016-12-18 12:40:11 -0200

    Is possible get only one message!

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,8 @@ class MessagesController < ApplicationController
   def index
     @message = Message.order("rand()").first
   end
+
+  def show
+    @message = Message.find(params['id'])
+  end
 end
